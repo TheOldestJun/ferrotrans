546adb72d0e9ac401a06a3a0ccc687c7dc578416
Stanislav Pukha <s.v.pukha@gmail.com>
2023-10-19 20:34:48 +0300

    ui fix

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,17 +2,14 @@ import { Container, Modal, Box, CircularProgress } from "@mui/material";
 
 const Info = ({ message }) => {
   return (
-    <Modal
-      open={true}
+    <CircularProgress
       style={{
         position: "absolute",
         top: "50%",
         left: "50%",
         zIndex: 10,
       }}
-    >
-      <CircularProgress />
-    </Modal>
+    />
   );
 };
 
