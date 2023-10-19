1c5c47736dc7cdb4098653cb524439767eddbcba
Stanislav Pukha <s.v.pukha@gmail.com>
2023-10-19 20:04:59 +0300

    minor fix - no version

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,21 +1,18 @@
-import { Modal, CircularProgress } from "@mui/material";
+import { Container, Modal, Box, CircularProgress } from "@mui/material";
 
 const Info = ({ message }) => {
   return (
-    <>
-      <Modal
-        open={true}
-        aria-label="modal-info"
-        aria-description="info"
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+    <Container>
+      <CircularProgress
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          zIndex: 10,
+          opacity: 0.5,
         }}
-      >
-        <CircularProgress />
-      </Modal>
-    </>
+      />
+    </Container>
   );
 };
 
