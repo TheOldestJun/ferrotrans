488333bf1b113dce308bf05dee122961c42244a8
Stanislav Pukha <s.v.pukha@gmail.com>
2023-10-19 20:21:12 +0300

    bug fix

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,17 +2,17 @@ import { Container, Modal, Box, CircularProgress } from "@mui/material";
 
 const Info = ({ message }) => {
   return (
-    <Container>
-      <CircularProgress
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          zIndex: 10,
-          opacity: 0.5,
-        }}
-      />
-    </Container>
+    <Modal
+      open={true}
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        zIndex: 10,
+      }}
+    >
+      <CircularProgress />
+    </Modal>
   );
 };
 
